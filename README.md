09eee4e06951872367f90d4d095633b92d2a1d7e
Filip Dąbek <filedil@gmail.com>
2019-05-19 19:02:55 +0200

    Fix formatting and then deprecate project

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # vim-sftpsync
+
+\[**DEPRECATED**\] I switched to VS Code and Sublime Text, so there will not be any updates.
+
 Upload files to remote server via sftp in Vim.
 
 This plugin allows you to send a file from a local project to a remote server using SFTP protocol. Unlike other plugins I've looked at, this one doesn't start external processes, but uses the Vim's python interface and python SSH library, [Paramiko](http://www.paramiko.org/). That way start times are shorter and you can reuse open connections, so subsequent uploads are even faster (I've measured 0.1s and lower). Makes it great for hooking to an autocommand like `BufWritePost`.
 
-Requirements
-------------
+## Requirements
+
 - Vim 8 or higher with +python3 and +timers (Python 2 support might be added later)
 - Python 3 with Paramiko library installed
 
-Installation
-------------
+## Installation
+
 If you use [vim-plug](https://github.com/junegunn/vim-plug), then add the following line to your vimrc file:
 
 ```vim
@@ -22,69 +25,66 @@ and then use `:PlugInstall`. Or, you can use any other plugin manager such as
 [neobundle](https://github.com/Shougo/neobundle.vim), or
 [pathogen](https://github.com/tpope/vim-pathogen).
 
-Basic configuration
--------------------
+## Basic configuration
+
 There are two important variables:
 
-* `g:sftpsync_private_key_file` - point this to your ssh identity (private key) if you don't want to keep passwords in configuration
+- `g:sftpsync_private_key_file` - point this to your ssh identity (private key) if you don't want to keep passwords in configuration
 
-    Example:
-    ```vim
-    let g:sftpsync_private_key_file = 'C:/Cygwin64/home/$USERNAME/.ssh/id_rsa'
-    ```
+  Example:
 
+  ```vim
+  let g:sftpsync_private_key_file = 'C:/Cygwin64/home/$USERNAME/.ssh/id_rsa'
+  ```
 
-* `g:sftpsync_projects` - where to send which files
+- `g:sftpsync_projects` - where to send which files
 
-    This is a list of "projects" that will match a source pattern (e.g. a local directory) to one or more destinations.
+  This is a list of "projects" that will match a source pattern (e.g. a local directory) to one or more destinations.
 
-    Example:
-    ```vim
-    let g:sftpsync_projects = {
-    \    'myproject': {
-    \        'source': '^.+/Projects/(Work/)?([a-z0-9]+)_myproject',
-    \        'destination': {
-    \            'dev': {
-    \                'directory': '/home/www/\2_myproject',
-    \                'hosts': ['user@giraffe.example.com'],
-    \            },
-    \            'test': {
-    \                'directory': '/home/www/myproject',
-    \                'hosts': ['user@hippo.example.com', 'user:p4ssw0rd@lion.example.com'],
-    \            },
-    \        }
-    \    },
-    \}
-    ```
+  Example:
 
-    This configuration will tell the plugin to:
-    - send any file from `*/Projects/test1_myproject` to `/home/www/test1_myproject` on `giraffe.example.com`,
+  ```vim
+  let g:sftpsync_projects = {
+  \    'myproject': {
+  \        'source': '^.+/Projects/(Work/)?([a-z0-9]+)_myproject',
+  \        'destination': {
+  \            'dev': {
+  \                'directory': '/home/www/\2_myproject',
+  \                'hosts': ['user@giraffe.example.com'],
+  \            },
+  \            'test': {
+  \                'directory': '/home/www/myproject',
+  \                'hosts': ['user@hippo.example.com', 'user:p4ssw0rd@lion.example.com'],
+  \            },
+  \        }
+  \    },
+  \}
+  ```
 
-        if the chosen target is 'dev'
-    - send any file from `*/Projects/Work/test2_myproject` to `/home/www/test2_myproject` on `giraffe.example.com`,
+  This configuration will tell the plugin to:
 
-        if the chosen target is 'dev'
-    - send any file from `*/Projects/test1_myproject` to `/home/www/myproject` on both `hippo` and `lion` servers,
+  - send any file from `*/Projects/test1_myproject` to `/home/www/test1_myproject` on `giraffe.example.com`, if the chosen target is 'dev'
+  - send any file from `*/Projects/Work/test2_myproject` to `/home/www/test2_myproject` on `giraffe.example.com`, if the chosen target is 'dev'
+  - send any file from `*/Projects/test1_myproject` to `/home/www/myproject` on both `hippo` and `lion` servers, if the chosen target is 'test'
+  - etc.
 
-        if the chosen target is 'test'
-    - etc.
+## Basic usage
 
-Basic usage
------------
 ```vim
 :call sftpsync#Upload([filename], [target])
 ```
 
 Arguments:
-* `filename` - full path to the uploaded file
-* `target` - alias of the target server(s), from `g:sftpsync_projects`
+
+- `filename` - full path to the uploaded file
+- `target` - alias of the target server(s), from `g:sftpsync_projects`
 
 If `target` is not set, variable `b:sftpsync_target` might be used, if it exists.
 
 If `filename` is also not set, `expand('%:p')` will be used instead (path to currently open buffer).
 
-Shortcut and autocommand examples
---------------------
+## Shortcut and autocommand examples
+
 ```vim
 nnoremap <silent> <F12> :call sftpsync#Upload(expand('%:p'), 'test')<CR>
 nnoremap <silent> <F11> :call sftpsync#Cycle(['test', '', 'other_test'])<CR>
@@ -96,11 +96,10 @@ augroup sftpsync
 augroup END
 ```
 
-Other
------
+## Other
+
 There are few more configuration variables, functions and some statusline support. Please consult project's .vim files.
 
-License
--------
-vim-sftpsync is licensed under the *MIT License*.
+## License
 
+vim-sftpsync is licensed under the *MIT License*.
--- autoload/sftpsync/python/sftpsync.py
+++ autoload/sftpsync/python/sftpsync.py
@@ -1,5 +1,3 @@
-# -*- coding: UTF-8 -*-
-
 # ============================================================================
 # File:        sftpsync.py
 # Description: Upload files to remote server via sftp
@@ -12,32 +10,32 @@ import os
 import paramiko
 import posixpath
 import re
-import sys
 import traceback
 import vim
+
 
 class SftpSync(object):
     connections = {}
 
     def __init__(self):
         try:
-            self.host_keys_file = os.path.expandvars(vim.eval('g:sftpsync_host_keys_file'))
-            self.host_keys = paramiko.util.load_host_keys(os.path.expandvars(vim.eval('g:sftpsync_host_keys_file')))
+            self.host_keys_file = os.path.expandvars(vim.eval("g:sftpsync_host_keys_file"))
+            self.host_keys = paramiko.util.load_host_keys(os.path.expandvars(vim.eval("g:sftpsync_host_keys_file")))
         except FileNotFoundError:
             pass
 
         try:
-            self.pkey = paramiko.RSAKey.from_private_key_file(os.path.expandvars(vim.eval('g:sftpsync_private_key_file')))
+            self.pkey = paramiko.RSAKey.from_private_key_file(
+                os.path.expandvars(vim.eval("g:sftpsync_private_key_file"))
+            )
         except FileNotFoundError:
             self.pkey = None
 
-        self.socket_timeout = int(vim.eval('g:sftpsync_socket_timeout'))
+        self.socket_timeout = int(vim.eval("g:sftpsync_socket_timeout"))
         self.debug = False
-
 
     def escapeQuote(self, str):
         return "" if str is None else str.replace("'", "''")
-
 
     def printError(self, error):
         if type(error) != list:
@@ -49,19 +47,15 @@ class SftpSync(object):
             vim.command("echom '%s'" % self.escapeQuote(str(line)))
         vim.command("echohl None")
 
-
     def printDebug(self, message):
         if self.debug:
             print("DEBUG: %s" % message)
 
-
     def setStatus(self, status):
         vim.command("let b:sftpsync_status = '{}'".format(status))
 
-
     def purgeCache(self):
         self.connections = {}
-
 
     def upload(self, filename, target):
         if not os.path.isfile(filename):
@@ -69,10 +63,12 @@ class SftpSync(object):
             self.setStatus("error")
             return False
 
-        for (project_name, project) in vim.eval('g:sftpsync_projects').items():
+        for (project_name, project) in vim.eval("g:sftpsync_projects").items():
             try:
-                (destination, count) = re.subn(project['source'], project['destination'][target]['directory'], filename, flags=re.IGNORECASE)
-                destination_dir = os.path.dirname(destination) + '/'
+                (destination, count) = re.subn(
+                    project["source"], project["destination"][target]["directory"], filename, flags=re.IGNORECASE
+                )
+                destination_dir = os.path.dirname(destination) + "/"
             except KeyError:
                 self.printError("Project '%s' doesn't have target '%s'" % (project_name, target))
                 self.setStatus("error")
@@ -80,29 +76,31 @@ class SftpSync(object):
 
             if count > 0:
                 # raise Exception('test')
-                config = project['destination'][target]
+                config = project["destination"][target]
 
-                for host in config['hosts']:
-                    (username, hostname) = host.rsplit('@', 1)
+                for host in config["hosts"]:
+                    (username, hostname) = host.rsplit("@", 1)
                     try:
-                        (hostname, port) = hostname.rsplit(':', 1)
+                        (hostname, port) = hostname.rsplit(":", 1)
                     except ValueError:
                         port = 22
 
                     try:
-                        (username, password) = username.split(':', 1)
+                        (username, password) = username.split(":", 1)
                     except ValueError:
                         password = None
 
                     try:
                         # raise Exception('test')
-                        cache_key = '{}_{}'.format(project_name, host)
+                        cache_key = "{}_{}".format(project_name, host)
 
                         if cache_key in self.connections and self.connections[cache_key][0].get_transport().is_active():
                             (t, sftp) = self.connections[cache_key]
                         else:
                             if not self.pkey and not password:
-                                raise FileNotFoundError('Neither private key file nor password is set, please adjust configuration')
+                                raise FileNotFoundError(
+                                    "Neither private key file nor password is set, please adjust configuration"
+                                )
 
                             t = paramiko.SSHClient()
 
@@ -117,7 +115,7 @@ class SftpSync(object):
                                 username=username,
                                 password=password,
                                 pkey=self.pkey,
-                                timeout = self.socket_timeout,
+                                timeout=self.socket_timeout,
                             )
                             sftp = t.open_sftp()
 
@@ -127,8 +125,14 @@ class SftpSync(object):
                             sftp.put(filename, destination)
                         except FileNotFoundError:
                             # make all missing directiories
-                            path = "/".join(destination_dir.split('/')[0:len(project['destination'][target]['directory'].split('/'))])
-                            subdirs = destination_dir.split('/')[len(project['destination'][target]['directory'].split('/')):-1]
+                            path = "/".join(
+                                destination_dir.split("/")[
+                                    0 : len(project["destination"][target]["directory"].split("/"))
+                                ]
+                            )
+                            subdirs = destination_dir.split("/")[
+                                len(project["destination"][target]["directory"].split("/")) : -1
+                            ]
 
                             for subdir in subdirs:
                                 path = posixpath.join(path, subdir)
@@ -141,7 +145,7 @@ class SftpSync(object):
                             sftp.put(filename, destination)
 
                     except Exception:
-                        self.printError(traceback.format_exc().split('\n'))
+                        self.printError(traceback.format_exc().split("\n"))
                         self.setStatus("error")
                         return False
 
@@ -156,5 +160,4 @@ class SftpSync(object):
 
 
 sftpSync = SftpSync()
-__all__ = ['sftpSync']
-
+__all__ = ["sftpSync"]
